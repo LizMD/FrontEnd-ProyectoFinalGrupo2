d73b67b042a9c4b8a73bb026c1a9e628ad9018a6
Bairon00 <bairon97@icloud.com>
2022-10-05 23:33:42 +0000

    card lista

--- src/js/component/cardMedicos2.js
+++ src/js/component/cardMedicos2.js
@@ -2,13 +2,16 @@ import React from "react";
 
 export const CardMedicos2 = () => {
     return (
-        <div className="card col-4">
-            <img className="card-img-top" src="https://1.bp.blogspot.com/-6jWWd3JPS2U/WL2R41-rHeI/AAAAAAAABY0/dx9E-cg3c60QCXK8q0MjZJ04-rmKMciuwCEw/s1600/medico-crea-blog.jpg" />
-            <div class="card-img-overlay">
-                <h4 class="card-title">John Doe</h4>
-                <p class="card-text">especialidades especialidades especialidades</p>
-                <a href="#" class="btn btn-primary">agendar hora</a>
-            </div>
+        <div className="card col-4 fondo">
+            <div className="cuadro">
+            <img className="card-img-top mx-3 imagen" src="https://1.bp.blogspot.com/-6jWWd3JPS2U/WL2R41-rHeI/AAAAAAAABY0/dx9E-cg3c60QCXK8q0MjZJ04-rmKMciuwCEw/s1600/medico-crea-blog.jpg" />
+        </div>     
+        <div className=" contDatos">
+            <h3>Dr.Navia Ayala</h3>
+            <h4>Medico cirujano</h4>
+            </div>       
+                
+
         </div>
 
     )
