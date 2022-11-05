965656afa2dfe79f4ddd00325b22012c87d51e8d
Zauverer <cgomezloaiza@gmail.com>
2022-11-05 17:43:27 +0000

    tercer commit

--- src/js/views/recuperar.js
+++ src/js/views/recuperar.js
@@ -23,21 +23,26 @@ export const Recuperar = () => {
        
     }
     return (
+        
         <div>
+            <div className="row justify-content-center">
+                <div className="col-6">
+                    <div><div style={{ width: "300px" }} className="container">
+                        <form >
+                            <div className="form-group ">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email"  onChange={bb}/>
+                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <button type="submit" className="btn btn-primary" onClick={()=>actions.recuperar(email.email,history)} >Submit</button>   
+                        </form>
+                    </div>
+                </div>
+                </div>
+ 
+            </div>
 
-                    <div><div style={{ height: "500px" }} className="container">
-                    <form >
-                        <div class="form-group ">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email"  onChange={bb}/>
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                     
-                        <button type="submit" class="btn btn-primary" onClick={()=>actions.recuperar(email.email,history)} >Submit</button>
-                        
-                    </form>
-                </div>
-                </div>
+                    
                 
         </div>
 
