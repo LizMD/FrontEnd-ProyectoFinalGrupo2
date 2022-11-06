d4ea60fafc3393586969f022c9155b8c7508c855
mcfrank18 <fmperezg2010@gmail.com>
2022-11-06 14:42:16 +0000

    finalizando1

--- src/js/views/pending.js
+++ src/js/views/pending.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../styles/home.css";
+import pend from "../../img/pend.jpg";
 
 
 
@@ -14,35 +15,29 @@ export const Pending=()=>{
     
     return(
         
-        <div className="altura">
-            <Box textAlign="center" py={10} px={6}>
-      <br></br>
-      <br></br>
-      <Heading
-      className="altoletra"
-        display="inline-block"
-        as="h2"
-        bgGradient="linear(to-r, green.500, green.800)"
-        backgroundClip="text">
-        Su pago se encueentra pendiente!!
-      </Heading>
+      <div>
+      <Box textAlign="center" py={2} px={6}>
+        <Heading
+          display="inline-block"
+          bgGradient="linear(to-r, green.500, green.800)"
+          backgroundClip="text"
+        >
+          <img src={pend} className="card-img-top" alt="..."></img>
+        </Heading>
         <br></br>
         <br></br>
-      <Link to="/">
-      <Button
-        colorScheme="green"
-        bgGradient="linear(to-r, green.600, green.700, green.800)"
-        color="white"
-        variant="solid">
-        Volver al Inicio
-      </Button>
-      </Link>
-    </Box>
-      
-            
-         
-  
-
-        </div>
+        <Link to="/">
+          <Button
+            textAlign="center"
+            colorScheme="green"
+            bgGradient="linear(to-r, green.600, green.700, green.800)"
+            color="white"
+            variant="solid"
+          >
+            Volver al Inicio
+          </Button>
+        </Link>
+      </Box>
+    </div>
     )
 }
--- src/js/views/rechazado.js
+++ src/js/views/rechazado.js
@@ -1,48 +1,37 @@
 import React from "react";
 import "../../styles/home.css";
+import recha from "../../img/recha.jpg";
 
-
-
-import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
+export const Rechazado = () => {
+  //const pay = () => alert("vas a pagar")
 
-
-export const Rechazado=()=>{
-    
-    //const pay = () => alert("vas a pagar")
-    
-    return(
-        
-        <div className="altura">
-            <Box textAlign="center" py={10} px={10}>
-      <br></br>
-      <br></br>
-      <Heading
-      className="altoletra"
-        display="inline-block"
-        as="h2"
-        bgGradient="linear(to-r, green.500, green.800)"
-        backgroundClip="text">
-        Su pago fue rechazado!!
-      </Heading>
+  return (
+    <div>
+      <Box textAlign="center" py={10} px={6}>
+        <Heading
+          display="inline-block"
+          bgGradient="linear(to-r, green.500, green.800)"
+          backgroundClip="text"
+        >
+          <img src={recha} className="card-img-top" alt="..."></img>
+        </Heading>
         <br></br>
         <br></br>
-      <Link to="/">
-      <Button
-        colorScheme="green"
-        bgGradient="linear(to-r, green.600, green.700, green.800)"
-        color="white"
-        variant="solid">
-        Volver al Inicio
-      </Button>
-      </Link>
-    </Box>
-      
-            
-         
-  
-
-        </div>
-    )
-}
+        <Link to="/">
+          <Button
+            textAlign="center"
+            colorScheme="green"
+            bgGradient="linear(to-r, green.600, green.700, green.800)"
+            color="white"
+            variant="solid"
+          >
+            Volver al Inicio
+          </Button>
+        </Link>
+      </Box>
+    </div>
+  );
+};
